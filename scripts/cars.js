3516a25d588ad2b44b8fd536d3b152c440ea65ec
devgabribeiro <gaburieruti@gmail.com>
2023-06-02 01:20:00 -0300

    home changes and new tab added

---
+++ scripts/cars.js
@@ -0,0 +1,164 @@
+const supraMk1 = document.querySelector(".supra-mk1")
+const supraMk2 = document.querySelector(".supra-mk2")
+const supraMk3 = document.querySelector(".supra-mk3")
+const supraMk4 = document.querySelector(".supra-mk4")
+const supraMk5 = document.querySelector(".supra-mk5")
+
+const btnMk1 = document.querySelector("#btn-mk1")
+const btnMk2 = document.querySelector("#btn-mk2")
+const btnMk3 = document.querySelector("#btn-mk3")
+const btnMk4 = document.querySelector("#btn-mk4")
+const btnMk5 = document.querySelector("#btn-mk5")
+
+
+document.addEventListener("DOMContentLoaded", selectMk1)
+
+btnMk1.addEventListener("click", selectMk1)
+btnMk2.addEventListener("click", selectMk2)
+btnMk3.addEventListener("click", selectMk3)
+btnMk4.addEventListener("click", selectMk4)
+btnMk5.addEventListener("click", selectMk5)
+
+
+function selectMk1(){
+    supraMk1.style.display = "flex"
+
+    if(supraMk1.style.display = "flex"){
+        const btnMk1 = document.querySelector("#btn-mk1")
+        btnMk1.style.color = "#FF3131"
+        btnMk1.style.border = "#FF3131 2px solid"
+
+        supraMk2.style.display = "none"
+        btnMk2.style.color = ""
+        btnMk2.style.border = ""
+
+        supraMk3.style.display = "none"
+        btnMk3.style.color = ""
+        btnMk3.style.border = ""
+
+        supraMk4.style.display = "none"
+        btnMk4.style.color = ""
+        btnMk4.style.border = ""
+
+        supraMk5.style.display = "none"
+        btnMk5.style.color = ""
+        btnMk5.style.border = "" 
+
+        
+    } 
+}
+
+function selectMk2(){
+    supraMk2.style.display = "flex"
+
+    if(supraMk2.style.display = "flex"){
+        const btnMk2 = document.querySelector("#btn-mk2")
+        btnMk2.style.color = "#FF3131"
+        btnMk2.style.border = "#FF3131 2px solid"
+
+        supraMk1.style.display = "none"
+        btnMk1.style.color = ""
+        btnMk1.style.border = ""
+
+        supraMk3.style.display = "none"
+        btnMk3.style.color = ""
+        btnMk3.style.border = ""
+
+        supraMk4.style.display = "none"
+        btnMk4.style.color = ""
+        btnMk4.style.border = ""
+
+        supraMk5.style.display = "none"
+        btnMk5.style.color = ""
+        btnMk5.style.border = ""
+
+
+    }
+}
+
+function selectMk3(){
+
+    supraMk3.style.display = "flex"
+
+    if(supraMk3.style.display = "flex"){
+        const btnMk3 = document.querySelector("#btn-mk3")
+        btnMk3.style.color = "#FF3131"
+        btnMk3.style.border = "#FF3131 2px solid"
+
+        supraMk1.style.display = "none"
+        btnMk1.style.color = ""
+        btnMk1.style.border = ""
+
+        supraMk2.style.display = "none"
+        btnMk2.style.color = ""
+        btnMk2.style.border = ""
+
+        supraMk4.style.display = "none"
+        btnMk4.style.color = ""
+        btnMk4.style.border = ""
+
+        supraMk5.style.display = "none"
+        btnMk5.style.color = ""
+        btnMk5.style.border = ""
+
+
+    }
+}
+
+function selectMk4(){
+
+    supraMk4.style.display = "flex"
+
+    if(supraMk4.style.display = "flex"){
+        const btnMk4 = document.querySelector("#btn-mk4")
+        btnMk4.style.color = "#FF3131"
+        btnMk4.style.border = "#FF3131 2px solid"
+
+        supraMk1.style.display = "none"
+        btnMk1.style.color = ""
+        btnMk1.style.border = ""
+
+        supraMk2.style.display = "none"
+        btnMk2.style.color = ""
+        btnMk2.style.border = ""
+
+        supraMk3.style.display = "none"
+        btnMk3.style.color = ""
+        btnMk3.style.border = ""
+
+        supraMk5.style.display = "none"
+        btnMk5.style.color = ""
+        btnMk5.style.border = ""
+
+
+    }
+}
+
+function selectMk5(){
+
+    supraMk5.style.display = "flex"
+
+    if(supraMk5.style.display = "flex"){
+        const btnMk5 = document.querySelector("#btn-mk5")
+        btnMk5.style.color = "#FF3131"
+        btnMk5.style.border = "#FF3131 2px solid"
+
+        supraMk1.style.display = "none"
+        btnMk1.style.color = ""
+        btnMk1.style.border = ""
+
+        supraMk2.style.display = "none"
+        btnMk2.style.color = ""
+        btnMk2.style.border = ""
+
+        supraMk3.style.display = "none"
+        btnMk3.style.color = ""
+        btnMk3.style.border = ""
+
+        supraMk4.style.display = "none"
+        btnMk4.style.color = ""
+        btnMk4.style.border = ""
+
+
+    }
+}
